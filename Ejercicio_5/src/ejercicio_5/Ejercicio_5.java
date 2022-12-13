889ee531ae0823680e36d39ea157b6036a904186
Daniel Roa <droa@apgslt.com>
2022-12-12 23:07:41 -0500

    Ejercicio 5 - Corregido

--- Ejercicio_5/src/ejercicio_5/Ejercicio_5.java
+++ Ejercicio_5/src/ejercicio_5/Ejercicio_5.java
@@ -6,6 +6,7 @@ package ejercicio_5;
 
 import ejercicio_5.Entidades.Cuenta;
 import ejercicio_5.Servicio.servicioCuenta;
+import java.io.IOException;
 import java.util.Scanner;
 
 /**
@@ -19,9 +20,9 @@ public class Ejercicio_5 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Scanner leer = new Scanner(System.in);
+        Scanner leer = new Scanner(System.in).useDelimiter("\n");
         servicioCuenta sC = new servicioCuenta();
-        System.out.println("Creación de Cuenta: ");
+        
         Cuenta cuenta_dr= sC.crearCuenta();
         int option=-1;
         do{
@@ -33,15 +34,18 @@ public class Ejercicio_5 {
             System.out.println("4. Consultar Saldo");
             System.out.println("5. Consultar Datos");
             System.out.println("6. Salir");
-            System.out.println("Elija una Opción: ");
+            System.out.print("Elija una Opción: ");
             option = leer.nextInt();
         
+            clearScreen(); 
             switch(option){
                 case 1 -> sC.crearDeposito(cuenta_dr);
                 case 2 -> sC.crearRetiro(cuenta_dr);
                 case 3 -> sC.retiroRapido(cuenta_dr);
                 case 4 -> sC.consultarSaldo(cuenta_dr); 
-                case 5 -> sC.consultarDatos(cuenta_dr);          
+                case 5 -> sC.consultarDatos(cuenta_dr);
+                case 6 -> option=0;
+                default -> System.out.print("Opción no Existente, Intenta nuevamente.");
             }
             
         }while(option > 0);
@@ -49,5 +53,45 @@ public class Ejercicio_5 {
         
         
     }
+    
+    public static void clearScreen() {
+        //System.out.print("\033[H\033[2J");
+        //System.out.flush();
+    
+        /*
+        try {
+            final String os = System.getProperty("os.name");
+
+            if (os.contains("Windows")) {
+                Runtime.getRuntime().exec("cls");
+            } else {
+                Runtime.getRuntime().exec("clear");
+            }
+        } catch (final Exception e) {
+            //  Handle any exceptions.
+        }
+        */
+        
+        /*
+        try {
+            if (System.getProperty("os.name").contains("Windows")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                Runtime.getRuntime().exec("clear");
+            }
+        } catch (IOException | InterruptedException ex) {
+        }
+        */
+        
+        try {
+            if (System.getProperty("os.name").contains("Windows")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                System.out.print("\033\143");
+            }
+        } catch (IOException | InterruptedException ex) {
+        }
+    }
+
     
 }
